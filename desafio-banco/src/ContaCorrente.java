66be26d8748903855027617e28a8092082d99d9f
bhclira <brenohencastro@gmail.com>
2025-02-15 08:41:39 -0300

    Adiciona estrutura inicial do projeto Java com classes de banco e configuração do VS Code

---
+++ desafio-banco/src/ContaCorrente.java
@@ -0,0 +1,21 @@
+public class ContaCorrente {
+
+    // atributos
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    // métodos
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+
+}
---
+++ desafio-banco/src/ContaPoupanca.java
@@ -0,0 +1,20 @@
+public class ContaPoupanca {
+    
+    // atributos
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    // métodos    
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+}
