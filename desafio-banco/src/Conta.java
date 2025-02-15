6660830c51e5164ed102f7f47292dced151322c6
bhclira <brenohencastro@gmail.com>
2025-02-15 09:29:58 -0300

    Torna a classe Conta abstrata e implementa construtor para inicialização de agência e número

--- desafio-banco/src/Conta.java
+++ desafio-banco/src/Conta.java
@@ -1,43 +1,53 @@
 // eu posso deixar essa classe Conta abstrata "abstract class" porquê nao quero deixar ninguem criar um new Conta, apenas ContaCorrente e ContaPoupanca por que não faz sentido para minha aplicação
 
 public abstract class Conta implements IConta{
-       
-    // atributos
-       private int agencia;
-       private int numero;
-       private double saldo;
     
-       @Override
-       public void depositar(double valor) {
-         
-           
-       }
-   
-       @Override
-       public void sacar(double valor) {
-           
-           
-       }
-   
-       @Override
-       public void transferir(double valor, Conta contaDestino) {
-           
-           
-       }
+     // se eu criar uma conta, será 1, se eu criar outra conta será 2 e assim por diante
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+       
+    // atributos protected para que as classes filhas possam acessar
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    
+    // construtor mudado para a classe pai
+    public Conta() {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        
+        
+    }
+
+    @Override
+    public void sacar(double valor) {
+        
+        
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        
+        
+    }
 
 
-       // getters
-       public int getAgencia() {
-           return agencia;
-       }
-   
-       public int getNumero() {
-           return numero;
-       }
-   
-       public double getSaldo() {
-           return saldo;
-       }
+    // getters
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 
 
 }
--- desafio-banco/src/ContaCorrente.java
+++ desafio-banco/src/ContaCorrente.java
@@ -1,3 +1,5 @@
 public class ContaCorrente extends Conta {
 
+    
+
 }
